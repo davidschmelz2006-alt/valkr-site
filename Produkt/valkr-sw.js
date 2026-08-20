85d43a5f7d85013df80644438a7a9c66a82881b5
Valkr Publisher <noreply@valkr.local>
2026-08-20 17:30:16 +0200

    Ansicht aktualisiert 2026-08-20 17:30

--- Produkt/valkr-sw.js
+++ Produkt/valkr-sw.js
@@ -12,7 +12,7 @@
    Das alte Problem „haengt fuer immer auf einer alten Version" bleibt
    trotzdem geloest: der Cache-Name traegt die Version, beim Aktivieren
    fliegt alles Aeltere raus. */
-var CACHE = 'valkr-v16.5';
+var CACHE = 'valkr-v16.6';
 var ASSETS = [
   './valkr-hub.html',
   './valkr-identity.html',
